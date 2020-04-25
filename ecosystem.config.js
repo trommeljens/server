02b3c647beb15a50aa089b787e7a1f013bfff65d
Tobias Hegemann <tobias.hegemann@googlemail.com>
2020-04-25 18:13:16 +0200

    Updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: "digitalstage-server",
-    script: "dist/server.js",
+    script: "dist/main.js",
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
